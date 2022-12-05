e67a02e99df035cbe0c0d3b90fd8edc903475a9c
fabian-elmar <fabsiban09@gmail.com>
2022-12-05 11:57:15 +0100

    merged

--- DB/mapDB.js
+++ DB/mapDB.js
@@ -1,16 +1,6 @@
 import mongoose from "mongoose"
 
-/* const pointSchema = new mongoose.Schema({
-    type: {
-        type: String,
-        enum: ['Point'],
-        required: true
-    },
-    coordinates: {
-        type: [Number],
-        required: true
-    }
-}); */
+
 
 const MapSchema = new mongoose.Schema(
     {
@@ -45,59 +35,8 @@ MapSchema.pre("find", function () {
 
 MapSchema.index({ location: "2dsphere" });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 const SetPin = mongoose.model("mapPin", MapSchema);
-=======
-const Mappin = mongoose.model("mappin", MapSchema);
->>>>>>> 41aa54829eadcd16de83301a025cd333ef7cdfb8
-=======
-const SetPin = mongoose.model("mappin", MapSchema);
->>>>>>> 6f3ae1c34c95b44ec425eb65ed71956e6ea9708c
 
 export default SetPin
 
 
-// var message = new SetPin({
-//     user: "637c95291571bfe764f98ca4",
-//     location: {
-//         type: "Point",
-//         coordinates: [51.098948, 16.110492]
-//     },
-// });
-// message.save((err, message) => {
-//     if (err) console.log(err);
-//     console.log(message);
-// });
-
-
-// SetPin.find({
-//     location: {
-//         $near: {
-//             $maxDistance: 100000,
-//             $geometry: {
-//                 type: "Point",
-//                 coordinates: [52, 16]
-//             }
-//         }
-//     }
-// }).find((error, results) => {
-//     if (error) console.log(error);
-//     console.log(JSON.stringify(results, 0, 2));
-// });
-
-/* 
-SetPin.find({
-    location: {
-        $geoWithin: {
-            $box: [
-                [52 - 10, 16 - 10],
-                [52 + 10, 16 + 10],
-            ]
-        }
-    }
-}).find((error, results) => {
-    if (error) console.log(error);
-    console.log(JSON.stringify(results, 0, 2));
-});
- */
